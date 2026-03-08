b4a2a03d0972320fb044d67349d624fd6ef1b195
Eduardo563 <erios_23@alu.uabcs.mx>
2026-03-07 22:40:32 -0700

    Login and SignUp

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 * **Codigo de la pantalla de inicio:** [MainActivity.kt](app/src/main/java/com/example/pantallaprincipal/MainActivity.kt)
 * **Carpeta de recursos de imagenes:** [Carpeta de imagenes](app/src/main/res/drawable/)
+* **Codigo de la pantalla de Login:** [LoginScreen](app/src/main/java/com/example/pantallaprincipal/MainActivity.kt)
+* **Codigo de la pantalla de Sign Up:** [SignUpScreen](app/src/main/java/com/example/pantallaprincipal/MainActivity.kt)
--- app/src/main/java/com/example/pantallaprincipal/MainActivity.kt
+++ app/src/main/java/com/example/pantallaprincipal/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pantallaprincipal
 
 
+import android.R.attr.checked
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,24 +15,33 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,11 +69,12 @@ fun HomeScreen(modifier: Modifier = Modifier){
         modifier = Modifier.background(Color(0xff4f4ab1))
     ){
         Column(
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier  = Modifier.padding(16.dp)
+            modifier  = Modifier
+                .padding(16.dp)
                 .clip(RoundedCornerShape(12))
-                .background(Color(0xFFFFFFFF)  )
+                .background(Color(0xFFFFFFFF))
         ){
             Image(
                 painter  = painterResource(id = R.drawable.imagen_inicio),
@@ -130,5 +141,235 @@ fun HomeScreen(modifier: Modifier = Modifier){
 fun HomeScreenPreview(){
     PantallaPrincipalTheme {
         HomeScreen()
+    }
+}
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier.background(Color(0xff4f4ab1))
+    ){
+        Column(
+            verticalArrangement = Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier  = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(12))
+                .background(Color(0xFFFFFFFF))
+        ){
+            Image(
+                painter  = painterResource(id = R.drawable.login_access),
+                contentDescription = null,
+                modifier = Modifier.size(300.dp)
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 18.dp)){
+                Text("Login",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 40.sp)
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(12.dp)) {
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Name", modifier = Modifier.padding(start = 12.dp))
+                    var name by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = {name = it}
+                    )
+                }
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Email", modifier = Modifier.padding(start = 12.dp))
+                    var email by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = {email = it}
+                    )
+                }
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Password", modifier = Modifier.padding(start = 12.dp))
+                    var password by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = {password = it},
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+                }
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Forgot password?", modifier = Modifier.padding(start = 30.dp),
+                        color = Color(0xFF555555),
+                        fontSize = 12.sp)
+                }
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier
+                        .padding(end = 35.dp, bottom = 14.dp)
+                        .fillMaxWidth()) {
+
+                    Button( onClick = {},
+                        modifier = Modifier.size(width = 125.dp, height = 50.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xff4f4ab1)),
+                    ) {
+                        Text("Sign In",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp)
+                    }
+                }
+            }
+
+        }
+    }
+}
+@Composable
+@Preview (showBackground = true, showSystemUi = false)
+fun LoginScreenPreview(){
+    PantallaPrincipalTheme {
+        LoginScreen()
+    }
+}
+
+@Composable
+fun SignUpScreen(){
+    Box(
+        modifier = Modifier.background(Color(0xff4f4ab1))
+    ){
+        Column(
+            verticalArrangement = Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier  = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(12))
+                .background(Color(0xFFFFFFFF))
+        ){
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 18.dp)){
+                Text("Sign Up",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 40.sp)
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(12.dp)) {
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Email", modifier = Modifier.padding(start = 12.dp))
+                    var email by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = {email = it}
+                    )
+                }
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Name", modifier = Modifier.padding(start = 12.dp))
+                    var name by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = {name = it}
+                    )
+                }
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Password", modifier = Modifier.padding(start = 12.dp))
+                    var password by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = {password = it},
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+                }
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Confirm password", modifier = Modifier.padding(start = 12.dp))
+                    var password by remember { mutableStateOf("")}
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = {password = it},
+                        visualTransformation = PasswordVisualTransformation()
+                    )
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(vertical = 12.dp)
+                ){
+                    var checked by remember {mutableStateOf(false)}
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = { checked = it }
+                        )
+                        Text("I have read and accept\nthe terms and conditions",
+                            textAlign = TextAlign.Left)
+                    }
+                }
+
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier
+                        .padding(end = 35.dp, bottom = 14.dp)
+                        .fillMaxWidth()) {
+
+                    Button( onClick = {},
+                        modifier = Modifier.size(width = 125.dp, height = 50.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xff4f4ab1)),
+                    ) {
+                        Text("Sign Up",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp)
+                    }
+                }
+            }
+
+        }
+    }
+}
+@Composable
+@Preview (showBackground = true, showSystemUi = false)
+fun SignUpScreenPreview(){
+    PantallaPrincipalTheme {
+        SignUpScreen()
     }
 }
