50878647484ec2bee4aeec1350db622cf662eccf
Eduardo563 <erios_23@alu.uabcs.mx>
2026-03-11 09:10:08 -0700

    validaciones en campos de texto, Sign Up

--- app/src/main/java/com/example/pantallaprincipal/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/pantallaprincipal/screens/SignUpScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation.Companion.keyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
@@ -28,6 +30,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,6 +42,32 @@ import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun SignUpScreen(navController: NavController){
+
+    // Estado
+    var name by remember{mutableStateOf("")}
+    var email by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+
+    var nameError by remember { mutableStateOf(false) }
+    var emailError by remember{mutableStateOf(false)}
+    var phoneError by remember { mutableStateOf(false) }
+    var confirmPasswordError by remember {mutableStateOf(false) }
+
+    fun isValidEmail (email:String): Boolean{
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isValidName (name: String): Boolean{
+        return name.matches(Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$"))
+    }
+
+    fun isValidPhone (phone: String): Boolean{
+        return  phone.matches(Regex("^\\d{10}$"))
+    }
+
     Box(
         modifier = Modifier.background(Color(0xff4f4ab1))
     ){
@@ -54,7 +83,7 @@ fun SignUpScreen(navController: NavController){
                 verticalArrangement = Arrangement.spacedBy(20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(horizontal = 12.dp, vertical = 18.dp)){
-                Text("Sign Up",
+                Text(text = "Sign Up",
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Bold,
                     fontSize = 40.sp)
@@ -69,10 +98,15 @@ fun SignUpScreen(navController: NavController){
                     horizontalAlignment = Alignment.Start
                 ) {
                     Text("Email", modifier = Modifier.padding(start = 12.dp))
-                    var email by remember { mutableStateOf("")}
                     OutlinedTextField(
                         value = email,
-                        onValueChange = {email = it}
+                        onValueChange = {
+                            email = it
+                            emailError = !isValidEmail(it)
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Email
+                        )
                     )
                 }
                 Column(
@@ -80,22 +114,11 @@ fun SignUpScreen(navController: NavController){
                     horizontalAlignment = Alignment.Start
                 ) {
                     Text("Name", modifier = Modifier.padding(start = 12.dp))
-                    var name by remember { mutableStateOf("")}
                     OutlinedTextField(
                         value = name,
-                        onValueChange = {name = it}
-                    )
-                }
-                Column(
-                    verticalArrangement = Center,
-                    horizontalAlignment = Alignment.Start
-                ) {
-                    Text("Password", modifier = Modifier.padding(start = 12.dp))
-                    var password by remember { mutableStateOf("")}
-                    OutlinedTextField(
-                        value = password,
-                        onValueChange = {password = it},
-                        visualTransformation = PasswordVisualTransformation()
+                        onValueChange = {
+                            name = it
+                            nameError = !isValidName(it)}
                     )
                 }
 
@@ -103,12 +126,48 @@ fun SignUpScreen(navController: NavController){
                     verticalArrangement = Center,
                     horizontalAlignment = Alignment.Start
                 ) {
-                    Text("Confirm password", modifier = Modifier.padding(start = 12.dp))
-                    var password by remember { mutableStateOf("")}
+                    Text("Phone", modifier = Modifier.padding(start = 12.dp))
+                    OutlinedTextField(
+                        value = phone,
+                        onValueChange = {
+                            phone = it
+                            phoneError = !isValidPhone(it)},
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Phone
+                        )
+                    )
+                }
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Password", modifier = Modifier.padding(start = 12.dp))
                     OutlinedTextField(
                         value = password,
-                        onValueChange = {password = it},
-                        visualTransformation = PasswordVisualTransformation()
+                        onValueChange = {
+                            password = it },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        )
+                    )
+
+                }
+
+                Column(
+                    verticalArrangement = Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text("Confirm password", modifier = Modifier.padding(start = 12.dp))
+                    OutlinedTextField(
+                        value = confirmPassword,
+                        onValueChange = {
+                            confirmPassword = it
+                            confirmPasswordError = (it != password)},
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        )
                     )
                 }
 
@@ -117,7 +176,6 @@ fun SignUpScreen(navController: NavController){
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier.padding(vertical = 12.dp)
                 ){
-                    var checked by remember {mutableStateOf(false)}
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.Center
@@ -153,6 +211,8 @@ fun SignUpScreen(navController: NavController){
                                 fontSize = 18.sp)
                         }
                         Button( onClick = {},
+                            enabled = !nameError && !name.isBlank() && !emailError && phoneError &&
+                                        !confirmPasswordError && checked,
                             modifier = Modifier.size(width = 125.dp, height = 50.dp),
                             colors = ButtonDefaults.buttonColors(Color(0xff4f4ab1)),
                         ) {
